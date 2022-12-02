11f98cbbcf792fecf3ad4b23525113d9a763a870
Qbpoland <100943538+Qbpoland@users.noreply.github.com>
2022-12-02 15:05:46 +0100

    Add files via upload

---
+++ weather-api/weather-api/weather-api/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace weather_api
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.get_data = new System.Windows.Forms.Button();
+            this.wmo = new System.Windows.Forms.TextBox();
+            this.temp = new System.Windows.Forms.TextBox();
+            this.wind = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Obecna temperatura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Prędkośc wiatru";
+            this.label3.Click += new System.EventHandler(this.wind_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aktualna pogoda";
+            this.label2.Click += new System.EventHandler(this.wmo_Click);
+            // 
+            // get_data
+            // 
+            this.get_data.Location = new System.Drawing.Point(295, 72);
+            this.get_data.Name = "get_data";
+            this.get_data.Size = new System.Drawing.Size(131, 98);
+            this.get_data.TabIndex = 3;
+            this.get_data.Text = "pobierz";
+            this.get_data.UseVisualStyleBackColor = true;
+            this.get_data.Click += new System.EventHandler(this.get_data_Click);
+            // 
+            // wmo
+            // 
+            this.wmo.Location = new System.Drawing.Point(159, 108);
+            this.wmo.Name = "wmo";
+            this.wmo.ReadOnly = true;
+            this.wmo.Size = new System.Drawing.Size(100, 23);
+            this.wmo.TabIndex = 4;
+            this.wmo.TextChanged += new System.EventHandler(this.wmo_TextChanged);
+            // 
+            // temp
+            // 
+            this.temp.Location = new System.Drawing.Point(159, 72);
+            this.temp.Name = "temp";
+            this.temp.ReadOnly = true;
+            this.temp.Size = new System.Drawing.Size(100, 23);
+            this.temp.TabIndex = 5;
+            this.temp.TextChanged += new System.EventHandler(this.temp_TextChanged);
+            // 
+            // wind
+            // 
+            this.wind.Location = new System.Drawing.Point(159, 147);
+            this.wind.Name = "wind";
+            this.wind.ReadOnly = true;
+            this.wind.Size = new System.Drawing.Size(100, 23);
+            this.wind.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(172, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Data i godzina";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(295, 21);
+            this.date.Name = "date";
+            this.date.ReadOnly = true;
+            this.date.Size = new System.Drawing.Size(143, 23);
+            this.date.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.wind);
+            this.Controls.Add(this.temp);
+            this.Controls.Add(this.wmo);
+            this.Controls.Add(this.get_data);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Button get_data;
+        private TextBox wmo;
+        private TextBox temp;
+        private TextBox wind;
+        private Label label4;
+        private TextBox date;
+    }
+}
---
+++ weather-api/weather-api/weather-api/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace weather_api
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void wmo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wind_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void get_data_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync("https://api.open-meteo.com/v1/forecast?latitude=54.35&longitude=18.65&current_weather=true").Result;
+            JsonDocument document = JsonDocument.Parse(response);
+            JsonElement current_weather = document.RootElement.GetProperty("current_weather");
+            temp.Text = current_weather.GetProperty("temperature").ToString() + "℃";
+            wind.Text = current_weather.GetProperty("windspeed").ToString() + "m/s";
+            date.Text = current_weather.GetProperty("time").ToString();
+
+            switch (current_weather.GetProperty("weathercode").ToString()[0])
+            {
+                case '0':
+                    wmo.Text = "czyste niebo";
+                    break;
+                case '1':
+                    wmo.Text = "leekkie zachmurzenie";
+                    break;
+                case '2':
+                    wmo.Text = "średnie zachmurzenie ";
+                    break;
+                case '3':
+                    wmo.Text = "pochmurnie";
+                    break;
+                case '4':
+                    wmo.Text = "mgła";
+                    break;
+                case '5':
+                    wmo.Text = "mżawka";
+                    break;
+                case '6':
+                    wmo.Text = "opady deszczu";
+                    break;
+                case '7':
+                    wmo.Text = "Opady śniegu";
+                    break;
+                case '8':
+                    wmo.Text = "Ulewny deszcz";
+                    break;
+                case '9':
+                    wmo.Text = "Burza";
+                    break;
+
+            }
+
+
+
+
+
+        }
+
+        private void temp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wmo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
